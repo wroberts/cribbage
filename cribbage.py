6488baa56816015d4875abd38f6d044f943a2690
Will Roberts <will.roberts@rz.hu-berlin.de>
2016-12-24 17:52:39 +0100

    cribbage.py: begin roughing out game

--- cribbage.py
+++ cribbage.py
@@ -154,3 +154,232 @@ def n_choose_k(n,k):
 # pair 10 2 2
 # special jack
 # score 3
+
+
+# ------------------------------------------------------------
+# Cribbage Game
+
+class CribbagePlayer(object):
+    '''
+    Abstract base class for an object that plays a game of cribbage.
+    '''
+
+    def __init__(self):
+        '''Constructor.'''
+        pass
+
+    def discard(self, hand, is_dealer):
+        '''
+        Asks the player to select two cards from `hand` for discarding to
+        the crib.
+
+        Return is a list of two indices into the hand array.
+
+        Arguments:
+        - `hand`: an array of 6 card values
+        - `is_dealer`: a flag to indicate whether the given player is
+          currently the dealer or not
+        '''
+        raise NotImplementedError()
+
+class RandomCribbagePlayer(CribbagePlayer):
+    '''A CribbagePlayer that always makes a random move.'''
+
+    def __init__(self, ):
+        '''Constructor.'''
+        super(RandomCribbagePlayer, self).__init__()
+
+    def discard(self, hand, is_dealer):
+        '''
+        Asks the player to select two cards from `hand` for discarding to
+        the crib.
+
+        Return is a list of two indices into the hand array.
+
+        Arguments:
+        - `hand`: an array of 6 card values
+        - `is_dealer`: a flag to indicate whether the given player is
+          currently the dealer or not
+        '''
+        return random.sample(range(6), 2)
+
+class Game(object):
+    '''
+    An object representing a game of cribbage between two
+    CribbagePlayers.
+    '''
+
+    def __init__(self, players):
+        '''
+        Constructor.
+
+        Arguments:
+        - `players`: a list of two CribbagePlayer objects
+        '''
+        self.players = players
+        # scores start at zero
+        self.scores = [0, 0]
+        # The players cut for first deal, and the person who cuts the
+        # lowest card deals.
+        # Randomly pick one player to start.
+        self.turn = random.randrange(2)
+        self.target_score = 121
+        # a flag to cache the game state
+        self.over = False
+        # the deck of the current game round
+        self.deck = None
+        # the hands of the two players during the current game round
+        self.hands = None
+        # the crib of the current game round
+        self.crib = None
+        # the card value of the "starter" or "cut" card for the
+        # current game round
+        self.starter_card = None
+
+    def do_round(self, verbose=False):
+        '''
+        Executes a single round of cribbage.
+
+            Play proceeds through a succession of "hands", each hand
+            consisting of a "deal", "the play" and "the show." At any
+            time during any of these stages, if a player reaches the
+            target score (usually 121), play ends immediately with
+            that player being the winner of the game. This can even
+            happen during the deal, since the dealer can score if a
+            Jack is cut as the starter.
+
+        Returns True if the game is not over after the round; False if
+        the game is over (or was already over before the round).
+
+        Arguments:
+        - `verbose`:
+        '''
+        if verbose:
+            print('Starting new round')
+        if self.deal_round(verbose=verbose):
+            if self.play_round(verbose=verbose):
+                return self.show_round(verbose=verbose)
+        return True
+
+    def deal_round(self, verbose=False):
+        '''
+        Shuffles and deals the cards for a single round of cribbage.
+
+            The dealer shuffles and deals six cards to each player.
+            Once the cards have been dealt, each player chooses four
+            cards to retain, then discards the other two face-down to
+            form the "crib", which will be used later by the dealer.
+            At this point, each player's hand and the crib will
+            contain exactly four cards. The player on the dealer's
+            left cuts the deck and the dealer reveals the top card,
+            called the "starter" or the "cut". If this card is a Jack,
+            the dealer scores two points for "his heels."
+
+        Returns True if the game is not over after the deal; False if
+        the game is over (or was already over before the deal).
+
+        Arguments:
+        - `verbose`:
+        '''
+        if self.over:
+            return False
+        # create a new shuffled deck
+        self.deck = make_deck()
+        random.shuffle(self.deck)
+        # deal out 6 cards to each player
+        nondealer_hand = self.deck[:12][::2]
+        dealer_hand = self.deck[:12][1::2]
+        self.deck = self.deck[12:]
+        # self.turn indicates the player who is dealer
+        self.hands = ([nondealer_hand, dealer_hand] if self.turn
+                      else [dealer_hand, nondealer_hand])
+        if verbose:
+            print('Dealing cards')
+            self.display_state()
+        # ask players to select cards to discard to crib
+        self.crib = []
+        for idx, player in enumerate(self.players):
+            discard_idxs = player.discard(self.hands[idx], idx == self.turn)
+            # sanity checking
+            assert len(set(discard_idxs)) == 2
+            assert all(0 <= i < 6 for i in discard_idxs)
+            discard_idxs = set(discard_idxs)
+            discards = [c for i,c in enumerate(self.hands[idx]) if i in discard_idxs]
+            if verbose:
+                print('Player {} discards: '.format(idx+1), ' '.join(print_card(c) for c in sorted(discards)))
+            self.crib.extend(discards)
+            self.hands[idx] = [c for i,c in enumerate(self.hands[idx]) if i not in discard_idxs]
+        if verbose:
+            self.display_state()
+        # randomly cut a card from the deck to serve as the "starter"
+        self.starter_card = random.choice(self.deck)
+        # check for "his nibs"
+        starter_value, _starter_suit = split_card(self.starter_card)
+        if verbose:
+            print('Starter card is ', print_card(self.starter_card))
+        if starter_value == 10:
+            if verbose:
+                print('Dealer gets two points for his nibs')
+            if not self.award_points(self.turn, 2):
+                return False
+        return True
+
+    def award_points(self, player_idx, num_points):
+        '''
+        Awards `num_points` to player `player_idx`.
+
+        Returns True if neither player has yet reached `target_score`,
+        or False if one or more players have already finished the
+        game.
+
+        Arguments:
+        - `player_idx`:
+        - `num_points`:
+        '''
+        if self.over:
+            return False
+        # check that neither player is over target_score
+        if any(score >= self.target_score for score in self.scores):
+            self.over = True
+            return False
+        # add the points to the given player's score
+        self.scores[player_idx] += num_points
+        # check if that player is now over target_score
+        if self.scores[player_idx] >= self.target_score:
+            self.over = True
+            return False
+        return True
+
+    def play_round(self, verbose=False):
+        '''
+        Blah
+
+        Arguments:
+        - `verbose`:
+        '''
+        pass
+
+    def show_round(self, verbose=False):
+        '''
+        Blah
+
+        Arguments:
+        - `verbose`:
+        '''
+        pass
+
+    def display_state(self):
+        for idx in range(2):
+            print('Player {}{}  '.format(idx+1, '(D)' if idx == game.turn else '   '), end='')
+            if game.hands:
+                print(' '.join([print_card(c) for c in sorted(game.hands[idx])]), end='')
+            print('  {:3} Points'.format(game.scores[idx]), end='')
+            if game.crib and idx == game.turn:
+                print('  Crib', ' '.join([print_card(c) for c in sorted(game.crib)]), end='')
+            print()
+
+# testing
+player1 = RandomCribbagePlayer()
+player2 = RandomCribbagePlayer()
+game = Game([player1, player2])
+game.do_round(verbose=True)
