e8228c3ea7cd7b006992a4f5de4ba4de4e956f66
Will Roberts <will.roberts@rz.hu-berlin.de>
2017-01-16 18:25:02 +0100

    cribbage/expt.py: qlearning uses minibatches of size 32

--- cribbage/expt.py
+++ cribbage/expt.py
@@ -150,6 +150,7 @@ def make_dqlearner(store, name):
     # validation will be performed by playing cribbage against a random
     # player
     model.validation_routine(compare_dqlearner_to_random_player)
+    model.minibatch_size(32)
     model.num_epochs(1)
     return model
 
